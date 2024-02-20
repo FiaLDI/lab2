63721a3a0facbcc18f550ad87a547b108566afcd
FiaLDI <leon.youtube2017@yandex.ru>
2024-02-20 07:58:48 +0300

    add pep8

--- prog/ind.py
+++ prog/ind.py
@@ -14,55 +14,47 @@ def get_product():
     value = int(input("Стоимость товара: "))
 
     return {
-        'name_of_product':name_of_product,
-        'name_of_market':name_of_market,
-        'value':value
+        "name_of_product": name_of_product,
+        "name_of_market": name_of_market,
+        "value": value,
     }
 
-      
+
 def display_products(workers):
     """
     Отобразить список работников.
     """
     if workers:
-        line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-            '-' * 4,
-            '-' * 30,
-            '-' * 20,
-            '-' * 10
-        )
+        line = "+-{}-+-{}-+-{}-+-{}-+".format("-" * 4, "-" * 30, "-" * 20, "-" * 10)
         print(line)
         print(
-            '| {:^4} | {:^30} | {:^20} | {:^10} |'.format(
-                "№",
-                "Название продукта",
-                "Имя магазина",
-                "Стоимость"
+            "| {:^4} | {:^30} | {:^20} | {:^10} |".format(
+                "№", "Название продукта", "Имя магазина", "Стоимость"
             )
         )
         print(line)
         for idx, worker in enumerate(workers, 1):
             print(
-                '| {:>4} | {:<30} | {:<20} | {:>10} |'.format(
+                "| {:>4} | {:<30} | {:<20} | {:>10} |".format(
                     idx,
-                    worker.get('name_of_product', ''),
-                    worker.get('name_of_market', ''),
-                    worker.get('value', 0)
+                    worker.get("name_of_product", ""),
+                    worker.get("name_of_market", ""),
+                    worker.get("value", 0),
                 )
             )
         print(line)
-       
+
     else:
         print("Список продуктов пуст.")
-       
-       
+
+
 def select_products(products, find_name):
     """
     Выбрать продукт с заданным именем.
     """
     result = []
     for product in products:
-        if product.get('name_of_product') == find_name:
+        if product.get("name_of_product") == find_name:
             result.append(product)
 
     return result
@@ -92,34 +84,34 @@ def main():
     while True:
         command = input(">>> ").lower()
 
-        if command == 'exit':
+        if command == "exit":
             break
 
-        elif command == 'add':
+        elif command == "add":
             product = get_product()
             products.append(product)
             if len(products) > 1:
-                products.sort(key=lambda item: item.get('name', ''))
+                products.sort(key=lambda item: item.get("name", ""))
 
-        elif command == 'list':
+        elif command == "list":
             display_products(products)
 
-        elif command.startswith('info '):
-            parts = command.split(' ', maxsplit=1)
+        elif command.startswith("info "):
+            parts = command.split(" ", maxsplit=1)
             find_name = parts[1]
             selected = select_products(products, find_name)
             display_products(selected)
 
         elif command.startswith("save "):
             parts = command.split(maxsplit=1)
-            
+
             file_name = parts[1]
-            
+
             save_products(file_name, products)
-        
+
         elif command.startswith("load "):
             parts = command.split(maxsplit=1)
-            
+
             file_name = parts[1]
 
             workers = load_products(file_name)
@@ -128,6 +120,5 @@ def main():
             print(f"Неизвестная команда {command}", file=sys.stderr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
--- prog/validate_json.py
+++ prog/validate_json.py
@@ -5,20 +5,18 @@ import json
 from jsonschema import validate
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     schema = {
-        "type" : "object",
-        "properties" : {
-            "name_of_product" : {"type" : "string"},
-            "name_of_market" : {"type" : "string"},
-            "value" : {"type" : "number"},
+        "type": "object",
+        "properties": {
+            "name_of_product": {"type": "string"},
+            "name_of_market": {"type": "string"},
+            "value": {"type": "number"},
         },
     }
 
     with open("products.json", "r", encoding="utf-8") as fin:
         a = json.load(fin)
-        for i in a: 
-            validate(
-                instance=i, schema=schema
-            )
+        for i in a:
+            validate(instance=i, schema=schema)
